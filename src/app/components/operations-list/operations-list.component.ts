b1969fe4ffc0177a6b8aca4dff86e18c04d2e111
DESKTOP-TIDECLD\Lucas <lucas.arraigada93@gmail.com>
2020-10-25 11:41:04 -0300

    se crea componente operations-list para listar los ultimos 10 componentes

---
+++ src/app/components/operations-list/operations-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operations-list',
+  templateUrl: './operations-list.component.html',
+  styleUrls: ['./operations-list.component.css']
+})
+export class OperationsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
