46bfd211894bbb06985b59954dd0500d6e075f01
DESKTOP-TIDECLD\Lucas <lucas.arraigada93@gmail.com>
2020-10-25 20:25:26 -0300

    configuracion de app-module y app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { OperationsFormComponent } from './components/operations-form/operations-form.component';
+import { OperationsListComponent } from './components/operations-list/operations-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/operations-list',
+    pathMatch: 'full',
+  },
+  { path: 'operations-list', component: OperationsListComponent },
+  { path: 'operation/add', component: OperationsFormComponent },
+  { path: 'operation/edit/:id', component: OperationsFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
