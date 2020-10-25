b790681705dfab6bfb2e7e4cfd1e9250d8ae9931
DESKTOP-TIDECLD\Lucas <lucas.arraigada93@gmail.com>
2020-10-25 20:26:18 -0300

    se crea vista operation-list con su respectivos servicios

--- src/app/components/operations-list/operations-list.component.ts
+++ src/app/components/operations-list/operations-list.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 
+//IMPORT SERVICE
+import { OperationsService } from '../../service/operations.service';
+
 @Component({
   selector: 'app-operations-list',
   templateUrl: './operations-list.component.html',
-  styleUrls: ['./operations-list.component.css']
+  styleUrls: ['./operations-list.component.css'],
+  providers: [OperationsService]
 })
 export class OperationsListComponent implements OnInit {
+  operations: any = [];
+  balance: any;
+  filterOperation: any = '';
+  operationsType: any = [];
+  operationIncome: any = [];
+  operationExpense: any = [];
 
-  constructor() { }
+  constructor(private _operationService: OperationsService) {}
 
   ngOnInit(): void {
+    this.getAllOperations();
+    this.getBalance();
+    this.getOperationType();
   }
 
+  
+  getOperationType() {
+    this._operationService.getAllOperationsType().subscribe(
+      (response) => {
+        this.operationsType = response;
+        for(var i = 0; i < this.operationsType.length; i++){
+          this.operationsType[i].description;
+          if(this.operationsType[i].description == "Incomes"){
+            this.operationIncome.push(this.operationsType[i].description);
+          } else {
+            this.operationExpense.push(this.operationsType[i].description);
+          }
+        }
+      }, (error) => {}
+    );
+  }
+
+  getAllOperations() {
+    this._operationService.getAllOperations().subscribe(
+      (response) => {
+        this.operations = response;
+      },
+      (error) => {
+
+    });
+  }
+
+  getBalance(){
+    this._operationService.getBalance().subscribe(
+      response => {
+        this.balance = response;
+      }, error => {
+
+      }
+    )
+  }
+
+  deleteOperation(id){
+    this._operationService.deleteOperation(id).subscribe(
+      response => {
+        this.getAllOperations();
+        this.getBalance();
+      }, error => {
+
+      }
+    )
+  }
 }
